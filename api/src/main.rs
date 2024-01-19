e24f630786f26bdc17effdd868704a13559e39e7
asungy <62207329+asungy@users.noreply.github.com>
2024-01-19 00:47:18 -0500

    Daily push: 2024-01-19

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+
+    // TODO: Send email and password to API to create password in database
+    let user = warp::path("user").and(
+        warp::path("new").map(|| "Create new user")
+    );
+
+    let routes = warp::get().and(
+        user
+    );
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
